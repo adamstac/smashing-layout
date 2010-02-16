801b8779a879a67038d03ecd0c8581e60f0d3167
Adam Stacoviak <adam@gethandcrafted.com>
2010-02-15 23:51:18 -0600

    Initial version of Smashing Layout and the base styles to the example site (the making of a future theme of adamstacoviak.com)

--- src/configuration.rb
+++ src/configuration.rb
@@ -3,6 +3,8 @@ require 'compass'
 Compass.configuration.parse('config.rb')
 configuration.sass_options = Compass.sass_engine_options
 
+configuration.preview_server_port = 3040
+
 configuration.haml_options = {
   :format => :html5,
   :attr_wrapper => '"'
