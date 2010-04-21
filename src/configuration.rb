9088bb9ed58c855e785e14563e7c728329aa5620
Adam Stacoviak <adam@gethandcrafted.com>
2010-04-21 17:35:31 -0500

    Updated the config to run on port 3000

--- src/configuration.rb
+++ src/configuration.rb
@@ -3,7 +3,7 @@ require 'compass'
 Compass.configuration.parse('config.rb')
 configuration.sass_options = Compass.sass_engine_options
 
-configuration.preview_server_port = 3040
+configuration.preview_server_port = 3000
 
 configuration.haml_options = {
   :format => :html5,
