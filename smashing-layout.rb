ade2a03a47853b91be147bd9b21deb04bd45db0f
Adam Stacoviak <adam@adamstacoviak.com>
2010-08-16 16:53:03 -0500

    Update to the base_directory path

--- smashing-layout.rb
+++ smashing-layout.rb
@@ -1,3 +1,3 @@
 # path from the library file to where you're keeping your compass stuff.
-base_directory  = File.join(File.dirname(__FILE__), '..')
+base_directory  = File.join(File.dirname(__FILE__), '/')
 Compass::Frameworks.register('smashing-layout', :path => base_directory)
