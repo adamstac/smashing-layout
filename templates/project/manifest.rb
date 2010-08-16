4f0dcdf0b3977a3674d130dc07c004706da1b8e5
Adam Stacoviak <adam@gethandcrafted.com>
2010-08-16 12:43:21 -0500

    Updated the manifest file

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,1 +1,16 @@
+description "Smashing Layout"
+
 discover :all
+
+help %Q{
+This is a message that users will see if they type
+
+  compass help my_extension
+
+You can use it to help them learn how to use your extension.
+}
+
+welcome_message %Q{
+This is a message that users will see after they install this pattern.
+Use this to tell users what to do next.
+}
