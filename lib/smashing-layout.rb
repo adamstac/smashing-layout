ed536293f60f148d84d7e8dbb66eae9d6be3fd85
Adam Stacoviak <adam@stacoviak.com>
2011-09-19 04:15:59 -0500

    Bumped to 0.0.4

--- lib/smashing-layout.rb
+++ lib/smashing-layout.rb
@@ -1,11 +1,9 @@
 require 'compass'
-
-# path from the library file to where you're keeping your compass stuff.
 Compass::Frameworks.register("smashing-layout", :path => "#{File.dirname(__FILE__)}/..")
 
 module SmashingLayout
   
-  VERSION = "0.0.3".freeze
-  DATE = "2011-3-20".freeze
+  VERSION = "0.0.4".freeze
+  DATE = "2011-9-19".freeze
 
 end
